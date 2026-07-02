21b3d8663ca96d329ea5d8dca15c7228ca85132a
clickzent <clickzent@gmail.com>
2026-07-02 11:37:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "CLICKZENT Webhook Server is Running!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return "Webhook received!"
